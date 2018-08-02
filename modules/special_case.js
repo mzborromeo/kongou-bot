ed658f3cd355316e1b76d4498251ca5416026859
mzborromeo <mzborromeo@gmail.com>
2018-08-02 20:04:57 +0700

    Update special_case.js
    Added new spices

--- modules/special_case.js
+++ modules/special_case.js
@@ -5,6 +5,7 @@ module.exports = (discord,message,config,client,logger,message_type) => {
 		/*emi case 232221425329504257 */
 		try{
 			/*logger.debug("user matched");*/
+			var triggered = false;
 			var message_content = ""+message.content;
 			message_content = message_content.toLowerCase();
 			if(message_content.includes("emi is f2p") || 
@@ -19,7 +20,7 @@ module.exports = (discord,message,config,client,logger,message_type) => {
 				message_content.includes("emi's just a f2p") ||
 				message_content.includes("f2p") ||
 				message_content.includes("free to play") ||
-				message_content.includes("f2play")){
+				message_content.includes("f2play") ){
 				/*logger.debug("statement match trying to react");*/
 				(async function(msg){
 									var unicodes = ["🐳","🐋","🐙","🦑","🇼","🇭","🇦","🇱","🇪"];
@@ -28,6 +29,16 @@ module.exports = (discord,message,config,client,logger,message_type) => {
 										.catch(/*msge => logger.debug("react failed")*/);
 									}
 								})(message);
+				triggered = true;
+			}else if(message_content.includes("don't spend") || message_content.includes("didn't spend")){
+				message.channel.send("You mean whale hard?");
+				triggered = true;
+			}else if(message_content.includes("0 benefits")){
+				message.channel.send("*0 benefits*?Aren't you a mihoho share holder already?");
+				triggered = true;
+			}
+			if(triggered === true){
+				message.guild.members.get("232221425329504257").setNickname("Galactic Whale btw");
 			}
 		}catch(err){
 			/*logger.error(err);*/
