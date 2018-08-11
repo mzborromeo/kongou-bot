24642378a9666fce564e702df5451453990bf32d
mzborromeo <mzborromeo@gmail.com>
2018-08-12 01:50:05 +0800

    Update list_command.js
    Grouped global variables into one big object

--- modules/list_command.js
+++ modules/list_command.js
@@ -12,7 +12,6 @@ var embed_fields_proc = function(emote_arr,page_num){
 };
 
 var edit_message =  function(message,field_list){
-	/*console.log(message);*/
 	var is_next = false;
 	message.edit({embed:{
 			color: 7165476,
@@ -20,7 +19,6 @@ var edit_message =  function(message,field_list){
 			fields:field_list
 		}
 	}).then().catch(function(err){
-		/*console.log(err);*/
 	});
 	return;
 };
@@ -47,7 +45,6 @@ var send_message =  function(message,emote_list,field_list,page_switch_buttons,c
 			if(r.emoji.name === page_switch_buttons[0]){
 				current_page+= -1;
 				var field_list = embed_fields_proc(emote_list,current_page);
-				/*console.log(field_list.length);*/
 				edit_message(msg,field_list);				
 				msg.clearReactions().then(function(){
 					manage_react(msg,page_switch_buttons);
@@ -55,7 +52,6 @@ var send_message =  function(message,emote_list,field_list,page_switch_buttons,c
 			}else if(r.emoji.name === page_switch_buttons[1]){
 				current_page+= 1;
 				var field_list = embed_fields_proc(emote_list,current_page);
-				/*console.log(field_list.length);*/
 				edit_message(msg,field_list);				
 				msg.clearReactions().then(function(){
 					manage_react(msg,page_switch_buttons);
@@ -63,103 +59,107 @@ var send_message =  function(message,emote_list,field_list,page_switch_buttons,c
 			}		
 		});
 		collector.on('end', function(collected){
-			/*console.log("pagination end");*/
 			msg.clearReactions().then().catch();
 		});		
 	}).catch();
 	return;
 };
 
-module.exports = (discord,message,config,client,logger,message_type) => {
-	let Discord = discord;
+module.exports = (global_params,message_type) => {
+	let Discord = global_params.discord;
 	if(message_type.command_name === "list"){
-		if(message_type.parameters[0] === "server"){
-			try{
-				var server_list = [];
-				var embed_message = new Discord.RichEmbed();
-				client.guilds.forEach(function(value, key, map){
-					if(value.id !== "410450878714347521"){/*skip test server*/
-						server_list.push({name:"*"+value.name+"*",value:value.id,inline:"false"});	
-					}		
-				}); 	
-				message.channel.send({embed:{
-						color: 7165476,
-						title: "List of servers I'm currenty joined",
-						fields:server_list
-					}
-				}).then().catch();
-			}catch(err){
-				/*logger.error(err);*/
-			}
-		}else if(message_type.parameters[0] === "emotes"){
-				var last_message_id = undefined;
-				var page_switch_buttons = ["◀","▶"];
-				var emote_list = {};
-				message.channel.fetchMessages({ limit: 2})
-				.then(function(messages){
-					messages.forEach(function(value,key,map){
-						if(value.id !== message.id){
-							last_message_id = value.id;
-							logger.debug("last message id was "+last_message_id);
-							const emoji_list = client.emojis;
-							var react_id = "";
-							var emote_name = message_type.parameters[0];
-							var content_index = 1;
-							var page_num = 1;
-							
-							var content = [];
-							emoji_list.forEach(function(value,key,map){
-								/*logger.debug("id: "+value.id+" index: "+content_index+" page: "+page_num);*/
-								content.push({id:value.id,name: value.name,animated:value.animated});
-								content_index++
-								if(content_index > 25){
-									emote_list[page_num] = JSON.parse(JSON.stringify(content));
-									page_num++;
-									content.length = 0;
-									content_index = 1;
-								}
-							});	
-						}
-					});
-					var current_page = 1;
-					var field_list = embed_fields_proc(emote_list,current_page);
-					send_message(message,emote_list,field_list,page_switch_buttons,current_page);
-				})
-				.catch();		
-		}else if(message_type.parameters[0] === "emotedetail"){
-				var last_message_id = undefined;
-				var emote_list = {};
-				var embed_fields =[];
-				message.channel.fetchMessages({ limit: 2}).then(function(messages){
-					messages.forEach(function(value,key,map){
-						if(value.id !== message.id){
-							last_message_id = value.id;
-							const emoji_list = client.emojis;
-							emoji_list.forEach(function(value,key,map){
-								if(value.name ===  message_type.parameters[1]){
-									let animated_string = (value.animated === true)?"a":"";
-									embed_fields.push({name:"*"+value.id+"*",value:"emote: <"+animated_string+":"+value.name+":"+value.id+">\n"+"name: "+value.name,inline:"true"});
-								}
-							});
-						}
-					});
-					message.channel.send({embed:{
+		global_params.message.delete(0).then(function(msg){
+			if(message_type.parameters[0] === "server"){
+				try{
+					var server_list = [];
+					var embed_message = new Discord.RichEmbed();
+					global_params.client.guilds.forEach(function(value, key, map){
+						if(value.id !== "410450878714347521"){/*skip test server*/
+							server_list.push({name:"*"+value.name+"*",value:value.id,inline:"false"});	
+						}		
+					}); 	
+					global_params.message.channel.send({embed:{
 							color: 7165476,
-							title: "Emote Details",
-							fields:embed_fields
+							title: "List of servers I'm currenty joined",
+							fields:server_list
 						}
-					}).then(function(){
+					}).then().catch();
+				}catch(err){
+					global_params.logger.error(err);
+				}
+			}else if(message_type.parameters[0] === "emotes"){
+					var last_message_id = undefined;
+					var page_switch_buttons = ["◀","▶"];
+					var emote_list = {};
+					global_params.message.channel.fetchMessages({ limit: 1})
+					.then(function(messages){
+						messages.forEach(function(value,key,map){
+							if(value.id !== msg.id){
+								last_message_id = value.id;
+								global_params.logger.debug("last message id was "+last_message_id);
+								const emoji_list = global_params.client.emojis;
+								var react_id = "";
+								var emote_name = message_type.parameters[0];
+								var content_index = 1;
+								var page_num = 1;
+								
+								var content = [];
+								emoji_list.forEach(function(value,key,map){
+									/*global_params.logger.debug("id: "+value.id+" index: "+content_index+" page: "+page_num);*/
+									content.push({id:value.id,name: value.name,animated:value.animated});
+									content_index++
+									if(content_index > 25){
+										emote_list[page_num] = JSON.parse(JSON.stringify(content));
+										page_num++;
+										content.length = 0;
+										content_index = 1;
+									}
+								});	
+								global_params.logger.debug(emote_list[1]);
+							}
+						});
+						var current_page = 1;
+						var field_list = embed_fields_proc(emote_list,current_page);
+						send_message(msg,emote_list,field_list,page_switch_buttons,current_page);
+					})
+					.catch();		
+			}else if(message_type.parameters[0] === "emotedetail"){
+					var last_message_id = undefined;
+					var emote_list = {};
+					var embed_fields =[];
+					global_params.message.channel.fetchMessages({ limit: 1}).then(function(messages){
+						messages.forEach(function(value,key,map){
+							if(value.id !== msg.id){								
+								last_message_id = value.id;
+								global_params.logger.debug("last message id was "+last_message_id);
+								const emoji_list = global_params.client.emojis;
+								emoji_list.forEach(function(value,key,map){
+									global_params.logger.debug(value.name);
+									if(value.name ===  message_type.parameters[1]){
+										let animated_string = (value.animated === true)?"a":"";
+										embed_fields.push({name:"*"+value.id+"*",value:"emote: <"+animated_string+":"+value.name+":"+value.id+">\n"+"name: "+value.name,inline:"true"});
+									}
+								});
+							}
+						});
+						msg.channel.send({embed:{
+								color: 7165476,
+								title: "Emote Details",
+								fields:embed_fields
+							}
+						}).then(function(){
 
+						}).catch(function(){
+
+						});					
 					}).catch(function(){
 
-					});					
-				}).catch(function(){
-
-				});
-		}else if(message_type.parameters[0] === "emotesat"){
-			/*list all of emotes on a given server*/
-		}else if(message_type.parameters[0] === "duplicates"){
-			/*list all of duplicate emotes*/
-		}
+					});
+			}else if(message_type.parameters[0] === "emotesat"){
+				/*list all of emotes on a given server*/
+			}else if(message_type.parameters[0] === "duplicates"){
+				/*list all of duplicate emotes*/
+			}
+		}).catch();
 	}
 }
