4a233ed3ff8b9eb020392bb3e5a3c63037838283
mzborromeo <mzborromeo@gmail.com>
2018-08-12 01:52:42 +0800

    Update rename_command.js
    Grouped global variables into one big object

--- modules/rename_command.js
+++ modules/rename_command.js
@@ -1,21 +1,21 @@
 /*rename command*/
-module.exports = (discord,message,config,client,logger,message_type) => {
-	let Discord = discord;
-	if(message_type.command_name === "renameemote" && message.guild.id !== "392004770228862977"){
+module.exports = (global_params,message_type) => {
+	let Discord = global_params.discord;
+	if(message_type.command_name === "renameemote" && global_params.message.guild.id !== "392004770228862977"){
 		var emote_id = message_type.parameters[0];
 		var new_emote_name = message_type.parameters[1]; 
 		try{
-			const emoji_list = client.emojis;
+			const emoji_list = global_params.client.emojis;
 			emoji_list.forEach(function(value,key,map){
 				if(emote_id === value.id){
 					value.setName(new_emote_name).then(function(){
-						message.channel.send("Emote successfully renamed");
-					}).catch(/*message.channel.send("Error setting new emote name")*/);
+						global_params.message.channel.send("Emote successfully renamed");
+					}).catch();
 					
 				}
 			});
 		}catch(err){
-			logger.error(err);
+			global_params.logger.error(err);
 		}
 	}
 }
