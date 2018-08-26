5f26866cc77e2166ead36eba96cf4cb54cb59b8a
mzborromeo <mzborromeo@gmail.com>
2018-08-27 01:24:36 +0700

    modified bomb command to proccess multi mention

--- modules/fun_commands.js
+++ modules/fun_commands.js
@@ -2,7 +2,7 @@
 module.exports = {
 	check_names:function(command_name){
 		/*var module_names = ["shoot","bomb"];*/
-		var module_names = ["bomb"];
+		var module_names = ["bomb","release","kidnap"];
 		var name_validation = {has_match:false,matching_name:""};
 		if(module_names.indexOf(command_name) > -1){
 			name_validation = {has_match:true,matching_name:command_name};
@@ -69,12 +69,38 @@ module.exports = {
 
 				});	
 			}else if(mentioned_count > 1){
-				msg.channel.send("<@"+msg.author.id+">, sorry you can't bomb "+mentioned_count+" users atm.");
+				var mentioned_user = "";
+				msg.mentions.members.forEach(async function(value,key,map){
+					mentioned_user = value.id;
+					await msg.channel.send({embed:{
+							color: 7165476,
+							author: {
+								name:"Kaboom!",
+								icon_url:"https://cdn.discordapp.com/emojis/479273053977837590.gif"
+							},
+							description:"<@"+msg.author.id+"> used a "+bomb_emotes[random_index]+" on <@"+mentioned_user+">. <@"+mentioned_user+"> was hurt physically and mentally."  
+						}
+					}).then(function(){
+
+					}).catch(function(){
+
+					});						
+				});
 			}else if(mentioned_count <= 0 ){
-				msg.channel.send("<@"+msg.author.id+">, who should I bomb? You didn't mention anyone.");
+				msg.channel.send("<@"+msg.author.id+">, who should I bomb? You didn't mentioned any member. Mentioning roles and channels won't work.");
 			}	
 		}).catch(function(err){
 			global_params.logger.error(err);
+		});
+	},
+	release:function(global_params,global_settings,message_type){
+		global_params.message.delete(0).then(function(msg){
+			msg.channel.send("command is under construction");
+		});
+	},
+	kidnap:function(global_params,global_settings,message_type){
+		global_params.message.delete(0).then(function(msg){
+			msg.channel.send("command is under construction");
 		});
 	}
 }
